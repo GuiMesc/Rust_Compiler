21036471ab0e3555f008283b68f30b25ca37115b
unknown <gui.mesc@outlook.com>
2021-08-01 12:09:31 -0300

    alteracao sintatico

--- Sintatico.py
+++ Sintatico.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ply.yacc as yacc
 import ply.lex as lex
 from Lexico import tokens
@@ -18,25 +17,4 @@ while True:
         break
     if not s: continue
     result = parser.parse(s)
-=======
-import ply.yacc as yacc
-import ply.lex as lex
-from Lexico import tokens
-
-def p_start1(p):
-    '''
-    start: declaracao_variavel_externa
-    
-    '''
-
-parser = yacc.yacc()
-
-while True:
-    try:
-        s = input()
-    except EOFError:
-        break
-    if not s: continue
-    result = parser.parse(s)
->>>>>>> 2a6bab10b95896c458e84d81727aa7ad2511931e
     print(result)
